36f4061d8728ec63e70ae2f456de5631ce6e8e8c
SammyTBrains <josephsamuel034@gmail.com>
2023-08-30 13:23:09 +0100

    bt change

---
+++ constants/styles.ts
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#d9eafd",
+    primary100: "#afe0fc",
+    primary200: "#81cbf0",
+    primary400: "#2198d4",
+    primary500: "#046dc3",
+    primary700: "#065589",
+    primary800: "#032a64",
+    accent500: "#f78d0c",
+    error50: "#fcc4d0",
+    error500: "#9b0935",
+    gray500: "#32404a",
+    gray700: "#1c2830",
+  },
+};
