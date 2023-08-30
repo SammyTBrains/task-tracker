250b2d87006d7f5f6162dfac26de0b1ca6f82ffc
SammyTBrains <josephsamuel034@gmail.com>
2023-08-30 15:14:00 +0100

    nice

--- components/ExpensesOutput/ExpensesList.tsx
+++ components/ExpensesOutput/ExpensesList.tsx
@@ -1,11 +1,12 @@
 import { FlatList, Text } from "react-native";
 import { ExpenseType } from "../../type-utilities/type";
+import ExpenseItem from "./ExpenseItem";
 
 type Props = { expenses: ExpenseType[] };
 
 const ExpensesList = (props: Props) => {
   const renderExpenseItem = (itemData: { item: ExpenseType }) => (
-    <Text>{itemData.item.description}</Text>
+    <ExpenseItem {...itemData.item} />
   );
 
   return (
