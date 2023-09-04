9d107842313eaae96f929d9b99ccf9c113a7f587
SammyTBrains <josephsamuel034@gmail.com>
2023-09-04 21:44:01 +0100

    uw

--- components/ManageExpense/ExpenseForm.tsx
+++ components/ManageExpense/ExpenseForm.tsx
@@ -1,38 +1,91 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+
 import Input from "./Input";
+import { GlobalStyles } from "../../constants/styles";
+
+type FormValues = {
+  amount: string;
+  date: string;
+  description: string;
+};
+
+const initialValues: FormValues = {
+  amount: "",
+  date: "",
+  description: "",
+};
+
+const onSubmit = (values: FormValues) => {
+  console.log(values);
+};
+
+const validationSchema = yup.object().shape({
+  amount: yup.string().required(),
+  date: yup.string().required(),
+  description: yup.string().required(),
+});
 
 const ExpenseForm = () => {
-  const amountChangeHandler = () => {};
-
   return (
-    <View style={styles.form}>
-      <Text style={styles.title}>Your Expense</Text>
-      <View style={styles.inputsRow}>
-        <Input
-          style={styles.rowInputs}
-          label="Amount"
-          textInputConfig={{
-            keyboardType: "decimal-pad",
-            onChangeText: amountChangeHandler,
-          }}
-        />
-        <Input
-          style={styles.rowInputs}
-          label="Date"
-          textInputConfig={{
-            placeholder: "YYYY-MM-DD",
-            maxLength: 10,
-            onChangeText: () => {},
-          }}
-        />
-      </View>
-      <Input
-        label="Description"
-        textInputConfig={{
-          multiline: true,
-        }}
-      />
-    </View>
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+          <View style={styles.form}>
+            <Text style={styles.title}>Your Expense</Text>
+            <View style={styles.inputsRow}>
+              <View style={styles.rowInputs}>
+                <Input
+                  label="Amount"
+                  textInputConfig={{
+                    keyboardType: "decimal-pad",
+                    onChangeText: handleChange("amount"),
+                    onBlur: handleBlur("amount"),
+                    value: values.amount,
+                  }}
+                />
+                {errors.amount && (
+                  <Text style={styles.errorText}>{errors.amount}</Text>
+                )}
+              </View>
+              <View style={styles.rowInputs}>
+                <Input
+                  label="Date"
+                  textInputConfig={{
+                    placeholder: "YYYY-MM-DD",
+                    maxLength: 10,
+                    onChangeText: handleChange("date"),
+                    onBlur: handleBlur("date"),
+                    value: values.date,
+                  }}
+                />
+                {errors.date && (
+                  <Text style={styles.errorText}>{errors.date}</Text>
+                )}
+              </View>
+            </View>
+            <Input
+              label="Description"
+              textInputConfig={{
+                multiline: true,
+                onChangeText: handleChange("description"),
+                onBlur: handleBlur("description"),
+                value: values.description,
+              }}
+            />
+            {errors.description && (
+              <Text style={styles.errorText}>{errors.description}</Text>
+            )}
+            <Button onPress={() => handleSubmit()} title="Submit" />
+          </View>
+        )}
+      </Formik>
+    </>
   );
 };
 
@@ -53,5 +106,8 @@ const styles = StyleSheet.create({
   },
   rowInputs: {
     flex: 1,
+  },
+  errorText: {
+    color: GlobalStyles.colors.error500,
   },
 });
